7d7745d7ba26e089482e2914e777bd53fe084c8d
houylingung <houylingung@gmail.com>
2022-02-05 22:25:27 -0500

    push project

---
+++ Flixer2UITests/Flixer2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Flixer2UITestsLaunchTests.swift
+//  Flixer2UITests
+//
+//  Created by Houyling Ung on 2/5/22.
+//
+
+import XCTest
+
+class Flixer2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
