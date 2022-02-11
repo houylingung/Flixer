e75de59649d6eace2b24ba1fe35116a9a52d0e60
houylingung <houylingung@gmail.com>
2022-02-10 22:22:14 -0500

    Flix part 2

---
+++ Flixer2/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixer2
+//
+//  Created by Houyling Ung on 2/10/22.
+//
+
+import UIKit
+
+
+class MovieGridCell: UICollectionViewCell {
+    
+  
+    @IBOutlet weak var posterView: UIImageView!
+}
